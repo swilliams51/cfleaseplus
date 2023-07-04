85266596d1e3d9301a7770d729ef126d100de9ef
steve williams <sjwilliams51@gmail.com>
2023-07-04 10:21:48 -0400

    Initial Commit

---
+++ cfleaseplus/cfleaseplusApp.swift
@@ -0,0 +1,17 @@
+//
+//  cfleaseplusApp.swift
+//  cfleaseplus
+//
+//  Created by Steven Williams on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct cfleaseplusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
